7953cda74bcfeadf80b49b2edc91ff060ecfede6
Ryan Carr <ryan.g.carr@hotmail.com>
2023-03-22 20:20:58 -0400

    set basic model and views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+# Models
+from .models import Widget
+# from .forms import WidgetForm
 
 # Create your views here.
+
+class WidgetCreate(CreateView):
+    model = Widget
+    fields = '__all__'
+    success_url = '/'
+    
+def index(request):
+    return render(request, 'index.html')
